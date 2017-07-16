c551151a46455c196f0e48bc0320eb1e075dfd88
Eduardo Bastier <canaleduardotech@gmail.com>
2017-07-16 05:04:43 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ BOARD_KERNEL_SEPARATED_DT := true
 TARGET_CUSTOM_DTBTOOL := dtbTool_moto
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_CONFIG := potter_defconfig
+TARGET_KERNEL_CONFIG := elementalx_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
 
 # Audio
